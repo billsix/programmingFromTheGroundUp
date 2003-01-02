6200bea761cddde8c203fb41746480838e7b81cc
johnnyb <johnnyb>
2003-01-02 05:35:54 +0000

    Removed redundant entries (now in ValuesCh.xml) Changed INITIAL_REFCOUNT to 0.  Not sure why...

--- StateManagementFlowControl/commonsystem.s
+++ StateManagementFlowControl/commonsystem.s
@@ -2,15 +2,9 @@
 	.globl NULL_LIST
 	.equ NULL_LIST, 0
 
-	.globl REFCOUNT_POSITION
-	.equ REFCOUNT_POSITION, 0
-	.globl TYPE_POSITION
-	.equ TYPE_POSITION, 4
-	.globl BASE_SIZE
-	.equ BASE_SIZE, 8
 	.globl INITIAL_REFCOUNT
-	.equ INITIAL_REFCOUNT, 1
-	
+	.equ   INITIAL_REFCOUNT, 0
+
 	#OS-Specific
 	.globl LINUX_SYSCALL
 	.equ LINUX_SYSCALL, 0x80
