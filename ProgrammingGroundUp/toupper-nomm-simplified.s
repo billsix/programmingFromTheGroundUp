0b44a4214cd841e82b9cb38a53a1d3a715bd1a44
johnnyb <johnnyb>
2004-01-10 05:07:03 +0000

    Modified toupper to use new acronyms for system call names Modified toupper to save %ebp before reserving stack space Added review questions to files chapter Finished out files chapter Added link to additional info for system calls Added info about open flags to the counting chapter

--- ProgrammingGroundUp/toupper-nomm-simplified.s
+++ ProgrammingGroundUp/toupper-nomm-simplified.s
@@ -14,11 +14,11 @@
 #######CONSTANTS########
 
 	#system call numbers
-	.equ OPEN, 5
-	.equ WRITE, 4
-	.equ READ, 3
-	.equ CLOSE, 6
-	.equ EXIT, 1
+	.equ SYS_OPEN, 5
+	.equ SYS_WRITE, 4
+	.equ SYS_READ, 3
+	.equ SYS_CLOSE, 6
+	.equ SYS_EXIT, 1
 
 	#options for open() (look at /usr/include/asm/fcntl.h for
 	#                    various values.  You can combine them
@@ -50,26 +50,28 @@
 
 	#STACK POSITIONS
 	.equ ST_SIZE_RESERVE, 8
-	.equ ST_FD_IN, 0
-	.equ ST_FD_OUT, 4
-	.equ ST_ARGC, 8      #Number of arguments
-	.equ ST_ARGV_0, 12   #Name of program
-	.equ ST_ARGV_1, 16   #Input file name
-	.equ ST_ARGV_2, 20   #Output file name
+	.equ ST_FD_IN, -4
+	.equ ST_FD_OUT, -8
+	.equ ST_ARGC, 0      #Number of arguments
+	.equ ST_ARGV_0, 4   #Name of program
+	.equ ST_ARGV_1, 8   #Input file name
+	.equ ST_ARGV_2, 12   #Output file name
 
 	.globl _start
 _start:
 	###INITIALIZE PROGRAM###
-	subl  $ST_SIZE_RESERVE, %esp       #Allocate space for our pointers on the stack
+	#save the stack pointer
 	movl  %esp, %ebp
+	#Allocate space for our file descriptors on the stack
+	subl  $ST_SIZE_RESERVE, %esp       
 
 open_files:
 open_fd_in:
 	###OPEN INPUT FILE###
+	movl  $SYS_OPEN, %eax            #open syscall
 	movl  ST_ARGV_1(%ebp), %ebx  #input filename into %ebx
 	movl  $O_RDONLY, %ecx        #read-only flag
 	movl  $0666, %edx            #this doesn't really matter for reading
-	movl  $OPEN, %eax            #open syscall
 	int   $LINUX_SYSCALL         #call Linux
 
 store_fd_in:
@@ -77,10 +79,10 @@ store_fd_in:
 
 open_fd_out:
 	###OPEN OUTPUT FILE###
+	movl  $SYS_OPEN, %eax                  #open the file
 	movl  ST_ARGV_2(%ebp), %ebx        #output filename into %ebx
 	movl  $O_CREAT_WRONLY_TRUNC, %ecx  #flags for writing to the file
 	movl  $0666, %edx                  #mode for new file (if it's created)
-	movl  $OPEN, %eax                  #open the file
 	int   $LINUX_SYSCALL               #call Linux
 
 store_fd_out:
@@ -90,10 +92,10 @@ store_fd_out:
 read_loop_begin:
 
 	###READ IN A BLOCK FROM THE INPUT FILE###
+	movl  $SYS_READ, %eax
 	movl  ST_FD_IN(%ebp), %ebx     #get the input file descriptor
 	movl  $BUFFER_DATA, %ecx       #the location to read into
 	movl  $BUFFER_SIZE, %edx       #the size of the buffer
-	movl  $READ, %eax
 	int   $LINUX_SYSCALL           #Size of buffer read is
 	                               #returned in %eax
 
@@ -110,10 +112,10 @@ continue_read_loop:
 	popl  %ebx
 
 	###WRITE THE BLOCK OUT TO THE OUTPUT FILE###
+	movl  $SYS_WRITE, %eax
 	movl  ST_FD_OUT(%ebp), %ebx    #file to use
 	movl  $BUFFER_DATA, %ecx       #location of the buffer
 	movl  %eax, %edx               #size of the buffer
-	movl  $WRITE, %eax
 	int   $LINUX_SYSCALL
 	
 	###CONTINUE THE LOOP###
@@ -123,17 +125,17 @@ end_loop:
 	###CLOSE THE FILES###
 	#NOTE - we don't need to do error checking on these, because 
 	#       error conditions don't signify anything special here
+	movl  $SYS_CLOSE, %eax
 	movl  ST_FD_OUT(%ebp), %ebx
-	movl  $CLOSE, %eax
 	int   $LINUX_SYSCALL
 
+	movl  $SYS_CLOSE, %eax
 	movl  ST_FD_IN(%ebp), %ebx
-	movl  $CLOSE, %eax
 	int   $LINUX_SYSCALL
 
 	###EXIT###
+	movl  $SYS_EXIT, %eax
 	movl  $0, %ebx
-	movl  $EXIT, %eax
 	int   $LINUX_SYSCALL
 
 
