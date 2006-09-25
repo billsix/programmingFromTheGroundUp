6abe3ff7a22712fcbe5d87f0132904cc9eb9fbcb
johnnyb <johnnyb>
2006-09-25 03:42:21 +0000

    Final branch processor draft Rewrote transform.pl

--- Power5PGU/transform.pl
+++ Power5PGU/transform.pl
@@ -9,7 +9,7 @@ s!<\!DOCTYPE.*?>!!si;
 s!<(chapter|article)>!<dw-document xsi:noNamespaceSchemaLocation="http://dw.raleigh.ibm.com/developerworks/library/schema/4.0/dw-document-4.0.xsd" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">\n\n<dw-article local-site="worldwide" ratings-form="auto" related-contents="auto" toc="auto" skill-level="3">\n<id cma-id="" domino-uid="" content-id="" original="yes"/>\n<keywords content="FIXME -- ADD KEYWORDS" />\n\n<\!-- FIXME - update meta -->\n<meta-last-updated day="16" month="10" year="2004" initials="jlb"/>\n\n<content-area-primary name="linux" />\n!si;
 
 s!</title>\s*<para>(.*?)</para>!</title><abstract>$1</abstract><docbody>!si;
-s!</title>!</title>\n\n<author jobtitle="Director of Technology" company="New Media Worx" email="johnnyb@eskimo.com"  >\n<bio>Jonathan Bartlett is the author of the book <a href="http://www.cafeshops.com/bartlettpublish.8640017"><i>Programming from the Ground Up</i></a> which is an introduction to programming using Linux assembly language.  He is the lead developer at New Media Worx, developing web, video, kiosk, and desktop applications for clients.\n</bio>\n<name>Jonathan Bartlett</name>\n</author>\n\n<\!-- FIXME - update date published -->\n<date-published day="01" month="09" year="2004" />!si;
+s!</title>!</title>\n\n<author jobtitle="Director of Technology" company="New Medio Worx" email="johnnyb@eskimo.com"  >\n<bio>Jonathan Bartlett is the author of the book <a href="http://www.cafeshops.com/bartlettpublish.8640017"><i>Programming from the Ground Up</i></a> which is an introduction to programming using Linux assembly language.  He is the lead developer at New Medio, developing web, video, kiosk, and desktop applications for clients.\n</bio>\n<name>Jonathan Bartlett</name>\n</author>\n\n<\!-- FIXME - update date published -->\n<date-published day="01" month="09" year="2004" />!si;
 
 s!<table>\s*<title>(.*?)</title>!<heading refname="" type="minor" toc="no">$1</heading>\n<table>!gsi;
 s!<variablelist>\s*<title>(.*?)</title>!<heading refname="" type="minor" toc="no">$1</heading>\n<dl>!gsi;
@@ -58,6 +58,7 @@ sub make_figure
 EOF
 }
 s!<example>\s*<title>(.*?)</title>\s*<programlisting>(.*?)</programlisting>\s*</example>!make_code_section($1, $2)!gsie;
+s!<programlisting>(.*?)</programlisting>!<code type="section">$1</code>!gsie;
 s!<mediaobject>\s*<imageobject>\s*<imagedata\s*fileref=["'](.*?)['"].*?>\s*</imageobject>\s*<caption>\s*<para>\s*(.*?)\s*</para>\s*</caption>\s*</mediaobject>!make_figure($2, $1)!gsie;
 s!<(/)?para>!<${1}p>!gsi;
 s!<literal>!<code type="inline">!gsi;
