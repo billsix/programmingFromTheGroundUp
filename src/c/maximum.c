71113ee1e49985a59d1f98e602e8b99d0e4703bf
William Emerison Six <billsix@gmail.com>
2026-03-05 22:35:28 -0500

    formatting

--- src/c/maximum.c
+++ src/c/maximum.c
@@ -4,22 +4,24 @@
 int data_items[] = {3, 67, 34, 222, 45, 75, 54, 34, 44, 33, 22, 11, 66, 0};
 
 int maximum() {
-  int i = 0;
-  int max = data_items[0];
+    int i = 0;
+    int max = data_items[0];
 
-  while (data_items[i] != 0) {
-    if (data_items[i] > max) max = data_items[i];
-    i++;
-  }
-  return max;
+    while (data_items[i] != 0) {
+        if (data_items[i] > max) {
+            max = data_items[i];
+        }
+        i++;
+    }
+    return max;
 }
 
 __attribute__((noreturn)) void _start(void) {
-  int max = maximum();
-  __asm__ volatile("int $0x80"
-                   :
-                   : "a"(1),   // SYS_exit
-                     "b"(max)  // Exit code in ebx
-  );
-  __builtin_unreachable();  // Good practice with noreturn
+    int max = maximum();
+    __asm__ volatile("int $0x80"
+                     :
+                     : "a"(1),   // SYS_exit
+                       "b"(max)  // Exit code in ebx
+    );
+    __builtin_unreachable();  // Good practice with noreturn
 }
